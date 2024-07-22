e42b1a50a52fd5413d3e8b0a2ba601bceebf1646
Mark <cc947331424@icloud.com>
2024-07-22 14:31:33 +0800

    chore(custom): 文档部署流程调整

--- packages/docs/src/pages/guide/index.md
+++ packages/docs/src/pages/guide/index.md
@@ -1,1 +1,9 @@
-# Guide
+# 起步
+
+Mark-Design 崇尚极简主义，以最简单的方式构建页面
+
+## 安装
+
+```bash
+$ pnpm i mark-design --save
+```
--- packages/play/.storybook/preview.js
+++ packages/play/.storybook/preview.js
@@ -1,3 +1,5 @@
+import "mark-design/dist/index.css";
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
